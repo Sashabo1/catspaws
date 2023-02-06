cd56725c529cd68b2d203463aea85fed2b5254ab
Pavel Radkevich <pradkevich@eisgroup.com>
2023-02-06 21:41:07 +0200

    ДЗ 4, первая проверка

--- src/main/java/pl/sasha/src/hw4/Hw1.java
+++ src/main/java/pl/sasha/src/hw4/Hw1.java
@@ -3,7 +3,7 @@ package pl.sasha.src.hw4;
 public class Hw1 {
 
     public static void main(String[] args) {
-        double a = 10;
+        double a = 10;  // хорошо было бы назвать переменные более значимо, например initialDistance, sumDistance
         double b =  0;
 
         for (int i = 1; i <= 7; i++) {
--- src/main/java/pl/sasha/src/hw5/Hw2.java
+++ src/main/java/pl/sasha/src/hw5/Hw2.java
@@ -1,18 +1,22 @@
 package pl.sasha.src.hw5;
 import java.util.Scanner;
 
+// Работает отлично!
+// но код нужно отформатировать
 public class Hw2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите объем двигателя");
+        // хочется другое имя для переменной, не однобуквенное
         double v = sc.nextDouble();
 
         System.out.println("Страховались ли у нас раньше?");
+        // хочется другое имя для переменной
         String y = sc.next();
         if (y.equals("да") ) {
             v = v * 0.8;
         }
-        else {
+        else { // этот else не имеет особого смысла, можно и не писать
             v = v * 1;
         }
         System.out.println("Были ли ДТП за последний год?");
@@ -20,7 +24,7 @@ public class Hw2 {
         if (dtp.equals("да")) {
            v = v * 1.2;
         }
-        else {
+        else { // этот else не имеет особого смысла, можно и не писать
             v = v * 1;
         }
         System.out.println(v * 0.25);
